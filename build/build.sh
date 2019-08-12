9d9cb730de638759b49f30882d18f7c0989f6035
Esdras Beleza de Noronha <github@contato.esdrasbeleza.com>
2019-08-12 23:47:16 +0100

    Remove binaries, use github releases instead

--- build/build.sh
+++ build/build.sh
@@ -10,5 +10,9 @@ if [ -z "${GOARCH}" ]; then
     export GOARCH=amd64
 fi
 
+if [ ! -d "bin/" ]; then
+    mkdir bin
+fi
+
 go build -v -o bin/wait-for-signal-${GOOS}-${GOARCH}-${VERSION} -ldflags="-s -w" ../cmd/wait-for-signal
 go build -v -o bin/send-signal-${GOOS}-${GOARCH}-${VERSION} -ldflags="-s -w" ../cmd/send-signal
