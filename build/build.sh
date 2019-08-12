88901bdb322616a429fd79bd860e20423c8b5489
Esdras Beleza de Noronha <github@contato.esdrasbeleza.com>
2019-08-12 23:25:36 +0100

    Move binaries to build/

---
+++ build/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VERSION=0.1
+
+if [ -z "${GOOS}" ]; then
+    export GOOS=linux
+fi
+
+if [ -z "${GOARCH}" ]; then
+    export GOARCH=amd64
+fi
+
+go build -v -o bin/wait-for-signal-${GOOS}-${GOARCH}-${VERSION} ../cmd/wait-for-signal
+go build -v -o bin/send-signal-${GOOS}-${GOARCH}-${VERSION} ../cmd/send-signal
